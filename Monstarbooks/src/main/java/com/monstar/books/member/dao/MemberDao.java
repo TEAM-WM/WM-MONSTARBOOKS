745c2712c8dd8e79fb0693d5652fccde7e161438
liyeon <fldus612@gmail.com>
2023-08-25 18:10:45 +0900

    Feat: 회원가입구현과 비밀번호 암호화 - 선호장르 콤마로 나눠서 디비에 넣기

--- Monstarbooks/src/main/java/com/monstar/books/member/dao/MemberDao.java
+++ Monstarbooks/src/main/java/com/monstar/books/member/dao/MemberDao.java
@@ -1,7 +1,10 @@
 package com.monstar.books.member.dao;
 
+import java.util.ArrayList;
+
 import com.monstar.books.member.dto.MemberDto;
 
 public interface MemberDao {
 	public void insert(MemberDto dto);
+	public ArrayList<MemberDto> list();
 }//======== MemberDao ========
