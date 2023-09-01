24449cab2e150aefa6fac6e1c5364d73f0391883
LAPTOP-NH4SMV4Q\jsk97 <jsk970427@hanmail.net>
2023-09-01 10:48:51 +0900

    Feat: 도서리스트&상세리스트 배송예정일 리뷰 추가, 장바구니에 담기 추가

--- Monstarbooks/src/main/java/com/monstar/books/booklist/controller/BookListController.java
+++ Monstarbooks/src/main/java/com/monstar/books/booklist/controller/BookListController.java
@@ -1,5 +1,7 @@
 package com.monstar.books.booklist.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
@@ -7,11 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.monstar.books.booklist.sevice.BookListService;
 import com.monstar.books.booklist.sevice.BookListServiceList;
+import com.monstar.books.booklist.sevice.addCartCheckServiceList;
+import com.monstar.books.booklist.sevice.addCartServiceList;
 import com.monstar.books.booklist.sevice.BookDetailServiceList;
 import com.monstar.books.booklist.vopage.SearchVO;
+import com.monstar.books.cart.sevice.CartDeleteServiceList;
 
 @Controller
 public class BookListController {
@@ -22,21 +29,6 @@ public class BookListController {
 	@Autowired
 	private SqlSession session;
 
-	// 230818 / 리연 추가
-//	@RequestMapping("/ex/sample1")
-//	public String list(Model model) {
-//
-//		System.out.println("샘플입니다.");
-//
-//		model.addAttribute("sample", "샘플값");
-//
-//		service = new ExServiceList(session);
-//		service.execute(model);
-//
-//		return "common/ex/sample1";
-//
-//	}// list 종료
-	
 	// 230822 / 진성 추가
 	// 베스트셀러 리스트
 	@RequestMapping("/booklist/bestlist")
@@ -59,13 +51,53 @@ public class BookListController {
 	// 230823 진성 추가
 	// 도서 상세 페이지
 	@RequestMapping("/booklist/bookdetail")
-	public String bookdetail(HttpServletRequest request, Model model) {
+	public String bookdetail(HttpServletRequest request,SearchVO searchVO, Model model) {
 		model.addAttribute("request",request);
+		model.addAttribute("searchVO",searchVO);
 		
 		service = new BookDetailServiceList(session);
 		service.execute(model);
 		
 		return "common/booklist/bookdetail";
 	}
+	// 230825 진성 추가
+	// 도서 리뷰 페이징
+	@RequestMapping("/booklist/reviewPage")
+	public String reviewPage(HttpServletRequest request,SearchVO searchVO, Model model) {
+		model.addAttribute("request",request);
+		model.addAttribute("searchVO",searchVO);
+		
+		service = new BookDetailServiceList(session);
+		service.execute(model);
+		
+		return "common/booklist/reviewPage";
+	}
+	// 상세페이지에서 장바구니 추가
+	@RequestMapping(method=RequestMethod.POST, value="/addCart")
+	public void addCart(HttpServletRequest request,SearchVO searchVO, Model model) {
+		
+		System.out.println("addCartttttttttttttt.");
+		model.addAttribute("request",request);
+		
+		service = new addCartServiceList(session);
+		service.execute(model);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/addCartCheck")
+	public void cartDelete(HttpServletRequest request,
+			@RequestParam(value = "chbox[]") List<String> chArr, Model model) {
+		
+		System.out.println("addCartCheckkkkkkkkkk");
+		
+		model.addAttribute("request",request);
+		model.addAttribute("chArr",chArr);
+		
+		service = new addCartCheckServiceList(session);
+		service.execute(model);
+		
+//		return "redirect:";
+		
+	}// list 종료
+	
 	
 }// class 종료
