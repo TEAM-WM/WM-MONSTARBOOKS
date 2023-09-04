bd25cde6852255f65e8360e7afde992c05f73e66
li_yeon <fldus612@gmail.com>
2023-09-04 10:06:17 +0900

    Feat: 이메일 양식 추가

--- Monstarbooks/src/main/java/com/monstar/books/member/controller/EmailController.java
+++ Monstarbooks/src/main/java/com/monstar/books/member/controller/EmailController.java
@@ -45,8 +45,49 @@ public class EmailController extends Authenticator {
         	String ran = Integer.toString(rand.nextInt(10)); 
         	numStr += ran;
         }
-		String content="인증번호 : "+numStr;
-		
+        
+        
+        String content = "<div\n" + 
+        		"        style=\"background: #F7F8F9; margin: 0; padding: 15px; cursor: default; letter-spacing: -.03em; font-family: 'Apple SD Gothic Neo', Helvetica, arial, '나눔고딕', 'Nanum Gothic', '돋움', Dotum, Tahoma, Geneva, sans-serif;\">\n" + 
+        		"        <table cellspacing=\"0\" cellpadding=\"0\"\n" + 
+        		"            style=\"max-width: 680px; width: 100%; margin: 0 auto; border-collapse: collapse;\">\n" + 
+        		"            <tbody>\n" + 
+        		"                <tr>\n" + 
+        		"                    <td align=\"center\" valign=\"top\">\n" + 
+        		"                        <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"background-color: #000d82; color: #ffffff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: 'Nunito Sans', Helvetica, Roboto, Arial, sans-serif; border-radius: 10px 10px 0 0;\">\n" + 
+        		"                            <tr>\n" + 
+        		"                                <td style=\"padding: 25px 40px; display: block;\">\n" + 
+        		"                                    <h1 style=\"font-size: 25px; font-weight: 600; color: #ffffff; background-color: inherit; margin: 0; text-align: left;line-height: 1.2em;\">몬스타북스<br>인증번호</h1>\n" + 
+        		"                                </td>\n" + 
+        		"                            </tr>\n" + 
+        		"                        </table>\n" + 
+        		"                    </td>\n" + 
+        		"                </tr>\n" + 
+        		"                <tr>\n" + 
+        		"                    <td colspan=\"2\" style=\"padding: 40px 10px; background: white; border: 1px solid #E5E8EB;\">\n" + 
+        		"                        <div\n" + 
+        		"                            style=\"max-width: 490px; width: 100%; margin: 0 auto; font-size: 13px; color: #373a3c; line-height: 1.8em; text-align: center;\">\n" + 
+        		"                            <h1\n" + 
+        		"                                style=\"font-size: 28px; font-weight: 600; color: #000d82;; text-align: center; line-height: 1.4em; \">\n"+numStr + 
+        		"                            </h1>\n" + 
+        		"                        </div>\n" + 
+        		"                    </td>\n" + 
+        		"                </tr>\n" + 
+        		"\n" + 
+        		"            </tbody>\n" + 
+        		"\n" + 
+        		"                <tr>\n" + 
+        		"                    <td colspan=\"2\" style=\"text-align: left; padding: 29px 0 50px 0;\">\n" + 
+        		"                        <p style=\"padding: 14px 0 0; margin: 0; line-height: 1.8em; color: #808991; font-size: 11px;\">\n" + 
+        		"                            본 메일은 발신전용 메일로, 회신되지 않습니다.\n" + 
+        		"                        <p\n" + 
+        		"                            style=\"padding: 8px 0 0; margin: 0; color: #808991; font-size: 14px; font-family: Tahoma, helvetica; \">\n" + 
+        		"                            © MONSTARBOOKS Corp.</p>\n" + 
+        		"                    </td>\n" + 
+        		"                </tr>\n" + 
+        		"            </tbody>\n" + 
+        		"        </table>\n" + 
+        		"    </div>";	
 		
 		Properties p=new Properties();
 		p.put("mail.smtp.user", from);
