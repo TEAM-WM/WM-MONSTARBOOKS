8dccac7118848a8201d1d5fd1ae29819cc010660
joon879 <joon879@gmail.com>
2023-09-05 10:59:11 +0900

    Feat. 상품 수정

--- Monstarbooks/src/main/java/com/monstar/books/product/dao/ProductDao.java
+++ Monstarbooks/src/main/java/com/monstar/books/product/dao/ProductDao.java
@@ -13,15 +13,25 @@ import com.monstar.books.product.dto.BookDto;
 public interface ProductDao {
 	
 	public ArrayList<BookDto> list();//전체 상품 리스트 호출
-	public BookDto detail(String bookno);//상품 디테일 진입
+	public BookDto detail(int bookno);//상품 디테일 진입
+	public ArrayList<BookCategoryDto> selectCategory();//도서카테고리 정보 리스트 불러오기
+	
 	
 	public void insertBook(int bisbn, String btitle, String bsubtitle, 
 			String bpublisher, String bwriter, String btranslator, String bpdate,
-			int bprice, int bpricesell, int bdiscount, String bstatus, int bstock);//테이블 "도서" 정보 입력
-	public ArrayList<BookCategoryDto> selectCategory();//도서카테고리 정보 리스트
+			int bprice, int bpricesell, int bdiscount, String bstatus, int bstock);//테이블 "도서" 인서트
 	public void insertBookDetail(int bcategoryno, String bimg, String bimgdetail, 
+			String bdescription, int bpage, String bsize, String badge);//테이블 "도서디테일" 인서트
+	
+	
+	public void updateBook(int bookno, int bisbn, String btitle, String bsubtitle, String bpublisher,
+			String bwriter, String btranslator, String bpdate, int bprice, int bpricesell,
+			int bdiscount, String bstatus, int bstock);
+	public void updateBookDetail(int bookno, int bcategoryno, String bimg, String bimgdetail, 
 			String bdescription, int bpage, String bsize, String badge);
-//	public void insertBookAllData(BookDto dto);
+	
+//	public void deleteBook(int bookno);
+//	public void deleteBookDetail();
 	
 	
 	
