d7cddac41a8fae865d8ca37d87988b26da7cdeed
oreho96 <oreseongho@gamil.com>
2023-09-07 17:47:45 +0900

    Feat: 쿠폰관리 기능 추가중 -쿠폰리스트 화면 완성 -쿠폰 등록페이지 작성중 -쿠폰sql 테이블 새로 업데이트

---
+++ Monstarbooks/src/main/java/com/monstar/books/adcoupon/controller/AdCouponController.java
@@ -0,0 +1,64 @@
+package com.monstar.books.adcoupon.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.monstar.books.adcoupon.service.AdCouponListService;
+import com.monstar.books.adcoupon.service.AdCouponRegisterService;
+import com.monstar.books.adcoupon.service.AdCouponService;
+import com.monstar.books.adcoupon.service.AdCouponStatusUpdateService;
+import com.monstar.books.vopage.SearchVo;
+
+@Controller
+public class AdCouponController {
+
+	AdCouponService adcouponservice;
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	@RequestMapping("/admin/coupon/list")
+	public String adcouponList(HttpServletRequest request, Model model, SearchVo searchvo) {
+
+		System.out.println("adcouponList 호출");
+
+		model.addAttribute("request", request);
+		model.addAttribute("SearchVo", searchvo);
+
+		adcouponservice = new AdCouponListService(sqlSession);
+		adcouponservice.execute(model);
+
+		return "admin/coupon/list";
+	}
+
+	@RequestMapping("admin/coupon/updatecouponStatus")
+	public String adcouponUpdate(HttpServletRequest request, Model model) {
+		System.out.println("adcouponUpdate 호출");
+
+		model.addAttribute("request", request);
+
+		adcouponservice = new AdCouponStatusUpdateService(sqlSession);
+		adcouponservice.execute(model);
+
+		return "redirect:/admin/coupon/list";
+	}
+	
+	@RequestMapping("admin/coupon/register")
+	public String adcouponRegister(HttpServletRequest request, Model model) {
+		System.out.println("adcouponRegister 호출");
+		
+		model.addAttribute("request",request);
+		
+		adcouponservice = new AdCouponRegisterService(sqlSession);
+		adcouponservice.execute(model);
+		
+		return "admin/coupon/register";
+		
+	}
+
+}
